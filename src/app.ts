fc37cbc0be639918f25b8b37e3f0bc20b747dfa8
lucastheldl <lucastheldl@gmail.com>
2023-06-29 17:02:21 -0300

    chore: prisma setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+
+/* app.get("/home", async()=>{
+  const users = await prisma.users.findMany()
+  return users;
+}) */
