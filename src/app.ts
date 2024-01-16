7785bcff9fc1b2507ca338497dd5131009ccecf7
lucastheldl <lucastheldl@gmail.com>
2024-01-16 16:31:16 -0300

    feat: Create user route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,30 @@
+import { prisma } from "@lib/prisma";
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import z from "zod";
 
 export const app = fastify();
-
-const prisma = new PrismaClient();
-
 
 /* app.get("/home", async()=>{
   const users = await prisma.users.findMany()
   return users;
 }) */
+
+app.post("/users", async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+
+  return reply.status(201).send();
+});
