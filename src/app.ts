375569b4ede90ccf17f2c771e67e75167e100875
lucastheldl <lucastheldl@gmail.com>
2024-01-22 14:40:23 -0300

    feat: global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import { env } from "@env";
 import { appRoutes } from "@http/routes";
 import fastify from "fastify";
+import { ZodError } from "zod";
 
 export const app = fastify();
 
@@ -9,3 +11,19 @@ export const app = fastify();
 }) */
 
 app.register(appRoutes);
+// request removed since its not beeing used \/
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "validation error", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.log(error);
+  } else {
+    // TODO:
+  }
+
+  return reply.status(500).send({ message: "Internal server error" });
+});
